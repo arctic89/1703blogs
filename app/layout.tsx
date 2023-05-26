041124e29e37a72c6776d9e3244086aaec1b4506
Aleksander <avasiliev@conteq.ru>
2023-05-27 01:00:39 +0300

    изменил лайаут

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         <main className="container">{children}</main>
+        
         <Footer />
       </body>
     </html>
