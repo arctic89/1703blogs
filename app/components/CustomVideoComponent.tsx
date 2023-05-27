6a92a063c5c1f97ac338dbd9fa1c42916d3f6969
Aleksander <avasiliev@conteq.ru>
2023-05-27 17:03:15 +0300

    Проверка работы видео

--- app/components/CustomVideoComponent.tsx
+++ app/components/CustomVideoComponent.tsx
@@ -4,7 +4,7 @@ import styles from './style.module.css'
 export default function CustomVideoComponent() {
   return (
     <div className="video-container" >
-      <video autoPlay loop muted src={require('../../public/videos/1703.mp4')} className={styles.videoFrame} />
+      <video autoPlay loop muted src={'/videos/1703.mp4'} className={styles.videoFrame} />
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -6,17 +6,10 @@ const nextConfig = {};
 
 module.exports = nextConfig;
 
-// module.exports = {
-//   env: {
-//     ENV_DEV: "",
-//     ENV_BUILD: "https://1703blogs.vercel.app",
-//   },
-// };
-
 const withVideos = require("next-videos");
 
 module.exports = withVideos({
-  assetPrefix: process.env.NODE_ENV === 'production' ? process.env.ASSET_PREFIX : '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? process.env.ASSET_PREFIX : '',
 
   webpack(config, options) {
     return config;
