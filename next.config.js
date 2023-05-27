f13f112caa5b08bc1597288a64628ea11291a568
Aleksander <avasiliev@conteq.ru>
2023-05-27 16:19:38 +0300

    изменил next config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withVideos = require('next-videos')
 
 module.exports = withVideos({
   // basePath: '/main',
-  assetPrefix: 'https://1703blogs.vercel.app/',
+  assetPrefix: 'https://1703blogs.vercel.app',
 
   webpack(config, options) {
     return config
