acab83ad35999d9b6635e250d4f98e26dbdd7edd
Aleksander <avasiliev@conteq.ru>
2023-05-29 08:01:52 +0300

    добавил предупреждение

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,13 @@ export default function MainPage() {
       <h1 className={styles.hero_title}>1703 Life Style</h1>
       <section className="container">
         <div className={styles.hero}>
-          <span className={styles.text}>
+          {/* <span className={styles.text}>
             Привет, Это 1703! И такой заголовок у нас неспроста. 1703 это для
             тех кто живёт музыкой, для тех кто дышит музыкой, для тех кто не
             может прожить без колебаний динамиков ни дня.
+          </span> */}
+          <span className={styles.text}>
+            Сайт находится в разработке!
           </span>
         </div>
       </section>
